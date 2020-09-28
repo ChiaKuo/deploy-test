97c9f15791a954239bd332e48b62f389a4bc454b
Jason <youchia1991@gmail.com>
2020-09-28 11:39:19 -0400

    Updates

---
+++ precache-manifest.3b5ace0e098f00d75515d6f3f17a66a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6363351dabc8258028009f101aa5be17",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "64d6db66f41cf6c6fddd",
+    "url": "/deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5b720cb6ada735e792b",
+    "url": "/deploy-test/static/js/2.3bbb2c63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-test/static/js/2.3bbb2c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d6db66f41cf6c6fddd",
+    "url": "/deploy-test/static/js/main.b36f0fd2.chunk.js"
+  },
+  {
+    "revision": "23db0319da0c4e8793b9",
+    "url": "/deploy-test/static/js/runtime-main.d3bbeae5.js"
+  }
+]);
